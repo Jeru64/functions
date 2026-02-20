b94b712f5b884a6d5fe32fbd9bb873cbd214cde1
Jeru64 <cherish.jacob@gmail.com>
2026-02-20 16:49:57 +0400

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,7 @@
+def weather_condition():
+    spring="autumn"
+    autumn=spring
+    print("the weather is pleasant in:", spring)
+    print("the weather is the same in:", autumn)
+    
+weather_condition()
